70131afe384db74ab91b0e9a5cc93f29455ba370
MartR <juneyourtech@users.noreply.github.com>
2017-08-08 04:31:19 +0300

    Update uTube_force_Flash.user.js
    Changed message wording.

--- uTube_force_Flash.user.js
+++ uTube_force_Flash.user.js
@@ -5,7 +5,7 @@
 // @author        JuneYourTech | github.com/juneyourtech | and contributors
 // @updateURL     https://raw.githubusercontent.com/juneyourtech/GM_YT_Flash/master/uTube_force_Flash.user.js
 // @downloadURL   https://raw.githubusercontent.com/juneyourtech/GM_YT_Flash/master/uTube_force_Flash.user.js
-// @version       0.4.1
+// @version       0.4.1.1
 // @encoding      utf-8
 // @homepage      https://github.com/juneyourtech/GM_YT_Flash
 // @supportURL    https://github.com/juneyourtech/GM_YT_Flash/issues
@@ -143,7 +143,7 @@ function jump_to_next() {
    var nextvid = document.getElementsByClassName('currently-playing')[0].nextElementSibling.getElementsByTagName('a')[0].getAttribute('href');
 
    /* msg here */
-   notice('Moving to next playlist item in about 10 seconds from now...');
+   notice('Moving to next playlist within the next 10 seconds from now...');
    
    window.setTimeout(function() {
       window.location.href = nextvid;
